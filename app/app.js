ed86d6a612d8d5314ed3947346eaf0f3a80b0ea1
Taimoor <82448357+taimoorrkhan@users.noreply.github.com>
2025-03-17 13:40:49 +0000

    add dotenv configuration

--- app/app.js
+++ app/app.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import { configDotenv } from 'dotenv';
 const app = express();
+
+configDotenv();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -7,5 +10,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is Running on http://localhost:${PORT}`);
 });
