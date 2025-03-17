65dcb4f720ebfbd5d58d2620d70d2a232d40cb09
Taimoor <82448357+taimoorrkhan@users.noreply.github.com>
2025-03-17 17:24:21 +0000

    refactor app.js to establish MongoDB connection and improve server startup logic

--- app/app.js
+++ app/app.js
@@ -1,14 +1,40 @@
 import express from 'express';
-import { configDotenv } from 'dotenv';
+import { config} from 'dotenv';
+import mongoose from 'mongoose';
 const app = express();
 
-configDotenv();
+config();
 
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+           
+        });
+        console.log('MongoDB Connected');
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+};
+
+connectDB();
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+const startserver = async (PORT) => {
+   try {
+    await connectDB();
+    app.listen(PORT, () => {
+        console.log(`Server is Running on http://localhost:${PORT}`)
+    });
+
+   } catch (error) {
+       console.error(`Error: ${error.message}`);
+       process.exit(1);
+    
+   }
+};
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is Running on http://localhost:${PORT}`);
-});
+
+startserver(PORT);
