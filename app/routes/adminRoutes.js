04282c8642a3b25c4d5380746370e6ce65466676
Taimoor <82448357+taimoorrkhan@users.noreply.github.com>
2025-03-19 16:16:40 +0000

    add deleteMovie route to adminRoutes for movie deletion

--- app/routes/adminRoutes.js
+++ app/routes/adminRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
-const { addNewMovie } = require('../controllers/adminControllers');
+const { addNewMovie,deleteMovie } = require('../controllers/adminControllers');
 
 const router = express.Router();
 
 
 router.post('/addMovie', addNewMovie);
+router.delete('/deleteMovie/:id', deleteMovie);
+
+
 
 
 module.exports = router;
