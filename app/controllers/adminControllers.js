1ac6db5c5f08caab053fc75a39e6d60f296095fc
Taimoor <82448357+taimoorrkhan@users.noreply.github.com>
2025-03-19 16:14:56 +0000

    add updateMovie function to adminControllers for movie updates

--- app/controllers/adminControllers.js
+++ app/controllers/adminControllers.js
@@ -31,5 +31,40 @@ const addNewMovie =  async (req, res) => {
     }
 }
 
+const updateMovie = async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+    
+        try {
+            const movie = await Movie.findById(req.params.id);
+            if (!movie) {
+                return res.status(404).json({ message: 'Movie not found' });
+            }
+    
+            const { title, year, cast, genres, extract, thumbnail, thumbnail_width, thumbnail_height } = req.body;
+    
+            movie.title = title;
+            movie.year = year;
+            movie.cast = cast;
+            movie.genres = genres;
+            movie.extract = extract;
+            movie.thumbnail = thumbnail;
+            movie.thumbnail_width = thumbnail_width;
+            movie.thumbnail_height = thumbnail_height;
+    
+            await movie.save();
+            res.json(movie);
+        } catch (err) {
+            res.status(500).json({ message: err.message });
+        }
+    };
+    
+
+}
+
+
+
 
 module.exports = { addNewMovie };
