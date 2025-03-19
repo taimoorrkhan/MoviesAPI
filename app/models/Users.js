6b8df8ce687b5c5e0f70b17574d6b6fdfc4a6300
Taimoor <82448357+taimoorrkhan@users.noreply.github.com>
2025-03-19 11:20:03 +0000

    add User model with schema definition for name, email, password, and created_at

---
+++ app/models/Users.js
@@ -0,0 +1,23 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const UserSchema  = new mongoose.Schema({
+    name :{
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true,
+        unique
+    },
+    password : {
+        type : String,
+        required,
+        minlength : 6
+    },
+    created_at : {
+        type : Date,
+        default : Date.now
+    }
+    
+})
