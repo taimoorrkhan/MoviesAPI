c7750a2db26fe0266ba81bbcb8ea5ce914027576
Taimoor <82448357+taimoorrkhan@users.noreply.github.com>
2025-03-19 13:14:53 +0000

    add route to fetch a single movie by ID

--- app/routes/movieRoutes.js
+++ app/routes/movieRoutes.js
@@ -1,11 +1,14 @@
 
 const express = require('express');
 
-const { getMovies, } = require('../controllers/moviesController');
+const { getMovies, getMovie} = require('../controllers/moviesController');
 
 const router = express.Router();
 
 router.get('/movies', getMovies);
+router.get('/movies/:id', getMovie);
+
+
 
 
 module.exports = router;
