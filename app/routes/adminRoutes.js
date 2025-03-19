da61db151a9fb37af4665e248a19032e4858a5a0
Taimoor <82448357+taimoorrkhan@users.noreply.github.com>
2025-03-19 16:17:03 +0000

    add updateMovie route to adminRoutes for movie updates

--- app/routes/adminRoutes.js
+++ app/routes/adminRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
-const { addNewMovie,deleteMovie } = require('../controllers/adminControllers');
+const { addNewMovie,deleteMovie ,updateMovie} = require('../controllers/adminControllers');
 
 const router = express.Router();
 
 
 router.post('/addMovie', addNewMovie);
 router.delete('/deleteMovie/:id', deleteMovie);
+router.put('/updateMovie/:id', updateMovie);
 
 
 
