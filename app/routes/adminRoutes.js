33d9d516dd856d35072b1b98159a42da69cfdf1c
Taimoor <82448357+taimoorrkhan@users.noreply.github.com>
2025-03-20 11:02:11 +0000

    add authenticateAdmin middleware to admin routes for enhanced security

--- app/routes/adminRoutes.js
+++ app/routes/adminRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 
 const { addNewMovie,deleteMovie ,updateMovie} = require('../controllers/adminControllers');
+const authenticateAdmin = require('../utils/adminAuth');
 
 const router = express.Router();
 
 
-router.post('/addMovie', addNewMovie);
-router.delete('/deleteMovie/:id', deleteMovie);
-router.put('/updateMovie/:id', updateMovie);
+router.post('/addMovie',authenticateAdmin, addNewMovie);
+router.delete('/deleteMovie/:id',authenticateAdmin, deleteMovie);
+router.put('/updateMovie/:id',authenticateAdmin, updateMovie);
 
 
 
