90113b2020ad9b05e935115c2d5166e376010426
Taimoor <82448357+taimoorrkhan@users.noreply.github.com>
2025-03-20 11:08:38 +0000

    add registration validation middleware to ensure user input integrity

--- app/utils/validators.js
+++ app/utils/validators.js
@@ -8,3 +8,17 @@ const handleValidationErrors = (req, res, next) => {
     }
     next();
 };
+
+// let code  registerValidator
+
+const validateRegister = [
+    body('name').notEmpty().withMessage('Name is required'),
+    body('email').isEmail().withMessage('Valid email is required'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+    handleValidationErrors
+];
+
+
+module.exports = {
+    validateRegister
+};
