64c127dac987df29eac591b55e5f2cb2d9b3b7d0
Taimoor <82448357+taimoorrkhan@users.noreply.github.com>
2025-03-20 11:13:04 +0000

    add movie ID and search validation middleware for improved input integrity

--- app/utils/validators.js
+++ app/utils/validators.js
@@ -25,8 +25,26 @@ const validateLogin = [
     handleValidationErrors
 ];
 
+//movies validators below
+
+const validateMovieId = [
+    param('id').isMongoId().withMessage('Invalid movie ID'),
+    handleValidationErrors
+];
+
+
+const validateMovieSearch = [
+    query('title').optional().isString().withMessage('Title must be a string'),
+    query('genre').optional().isString().withMessage('Genre must be a string'),
+    query('year').optional().isInt({ min: 1900, max: new Date().getFullYear() })
+        .withMessage('Year must be a valid number between 1900 and the current year'),
+    query('cast').optional().isString().withMessage('Cast must be a string'),
+    handleValidationErrors
+];
 
 module.exports = {
     validateRegister,
-    validateLogin
+    validateLogin,
+    validateMovieId,
+    validateMovieSearch
 };
