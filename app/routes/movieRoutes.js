2ae09b201fc3510bac339bc5599e30f02c7077e7
Taimoor <82448357+taimoorrkhan@users.noreply.github.com>
2025-03-20 11:16:00 +0000

    add validation middleware for movie search and ID routes to enhance input integrity

--- app/routes/movieRoutes.js
+++ app/routes/movieRoutes.js
@@ -2,12 +2,13 @@
 const express = require('express');
 
 const { getMovies, getMovie,searchMovies} = require('../controllers/moviesController');
+const { validateMovieSearch, validateMovieId } = require('../utils/validators');
 
 const router = express.Router();
 
 router.get('/movies', getMovies);
-router.get('/movies/search', searchMovies);
-router.get('/movies/:id', getMovie);
+router.get('/movies/search',validateMovieSearch ,searchMovies);
+router.get('/movies/:id', validateMovieId,getMovie);
 
 
 
