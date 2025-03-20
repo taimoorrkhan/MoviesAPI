28b62970d388b11ae34108523e18ec00c4764584
Taimoor <82448357+taimoorrkhan@users.noreply.github.com>
2025-03-20 10:31:46 +0000

    add searchMovies route to movieRoutes for movie searching

--- app/routes/movieRoutes.js
+++ app/routes/movieRoutes.js
@@ -1,12 +1,13 @@
 
 const express = require('express');
 
-const { getMovies, getMovie} = require('../controllers/moviesController');
+const { getMovies, getMovie,searchMovies} = require('../controllers/moviesController');
 
 const router = express.Router();
 
 router.get('/movies', getMovies);
 router.get('/movies/:id', getMovie);
+router.get('/search', searchMovies);
 
 
 
