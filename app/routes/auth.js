3cd8c5f269e0375e9a051d9f490db271d5d2dfe8
Taimoor <82448357+taimoorrkhan@users.noreply.github.com>
2025-03-20 11:10:27 +0000

    add validation middleware to registration route for improved input integrity

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { userReg, userLogin } = require('../controllers/authController');
+const { validateRegister } = require('../utils/validators');
 const router = express.Router();
 
 
-router.post('/register', userReg);
+router.post('/register',validateRegister, userReg);
 router.post('/login', userLogin);
 
 module.exports = router;
