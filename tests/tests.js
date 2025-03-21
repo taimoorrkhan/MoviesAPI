412b3b333d20dd8c37622c9bd01bf8550c1266c9
Taimoor <82448357+taimoorrkhan@users.noreply.github.com>
2025-03-21 10:12:14 +0000

    add tests for user login and movie addition with admin authentication

--- tests/tests.js
+++ tests/tests.js
@@ -8,6 +8,18 @@ const testUserData = {
     email: "testuser@example.com",
     password: "testpassword123",
 };
+const testMovieData = {
+    title: "Test Movie",
+    year: 2021,
+    cast: "Test Actor",
+    genres: ["Action", "Adventure"],
+    extract: "Test Extract",
+    thumbnail: "https://example.com/test.jpg",
+    thumbnail_width: 200,
+    thumbnail_height: 300
+};
+const adminUser = { name: 'Admin User', email: 'admin@example.com', password: 'adminpassword', role: 'admin' };
+
 describe('User and Movie API Tests', function () {
 
     before(async () => {
@@ -49,7 +61,7 @@ describe('User and Movie API Tests', function () {
 
 
     // Login Test
-    
+
 it('should login the user successfully', async () => {
     await request(app)
         .post('/api/auth/register')
@@ -66,6 +78,67 @@ it('should login the user successfully', async () => {
     expect(res.body).to.have.property('message', 'Logged In');
     expect(res.body).to.have.property('token');
 });
+
+// wrong password
+
+it('should return error for invalid credentials', async () => {
+    const res = await request(app)
+        .post('/api/auth/login')
+        .send({
+            email: testUserData.email,
+            password: 'wrongpassword'
+        });
+    
+    expect(res.status).to.equal(400);  
+    expect(res.body).to.have.property('message', 'Invalid password');
+});
+
+// add movie test
+
+it('should add a new movie when the admin is authenticated', async () => {
+    // Register and login an admin
+    
+    await request(app)
+        .post('/api/auth/register')
+        .send(adminUser);
+    
+    const loginRes = await request(app)
+        .post('/api/auth/login')
+        .send({ email: adminUser.email, password: adminUser.password });
+    
+    const token = loginRes.body.token;
+    const res = await request(app)
+        .post('/api/admin/addMovie')
+        .set('Authorization', `Bearer ${token}`)
+        .send(testMovieData);
+
+    expect(res.status).to.equal(201);
+    expect(res.body).to.have.property('message', 'Movie added successfully');
+});
+
+//no token 
+it('should not allow adding a movie without JWT token', async () => {
+    const movieData = {
+        title: 'New Movie',
+        year: 2025,
+        cast: ['Actor A', 'Actor B'],
+        genres: ['Action'],
+        extract: 'An action-packed movie',
+        thumbnail: 'https://example.com/newmovie.jpg',
+        thumbnail_width: 220,
+        thumbnail_height: 326
+    };
+
+    const res = await request(app)
+        .post('/api/admin/addMovie')
+        .send(movieData);
+
+    expect(res.status).to.equal(401);  
+    expect(res.body).to.have.property('message', 'Access denied');
+});
+
+
+
 
 
 });
